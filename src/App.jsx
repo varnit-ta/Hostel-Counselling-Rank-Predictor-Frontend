86be8b5fb05df7cf62c850001105f612b0f169b0
Yuvin Raja <yuvinrajav@gmail.com>
2025-04-09 12:21:59 +0530

    change ui #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import TopperList from "./TopperList";
 import TopperUpdateForm from "./TopperUpdateForm";
 import StudentRegisterForm from "./StudentRegisterForm";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard"; // you'll create this next
 
 function App() {
   return (
-    <div className="max-w-3xl mx-auto p-6 space-y-8">
-      <StudentRegisterForm />
-      <TopperList />
-      <TopperUpdateForm />
-    </div>
+    <Routes>
+      <Route path="/" element={<StudentRegisterForm />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
